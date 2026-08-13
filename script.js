78f4aafcf9ecd03464c8a1bda455314e26570f3c
Shubham Gupta <195768730+SujalTesting@users.noreply.github.com>
2026-08-13 14:47:15 +0530

    Change video start time from 35 to 27 seconds

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function onYouTubeIframeAPIReady() {
       fs: 0,
       modestbranding: 1,
       playsinline: 1,
-      start: 35,
+      start: 27,
     },
     events: {
       onReady: () => {
